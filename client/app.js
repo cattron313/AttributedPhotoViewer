1e862abbf2d3c176a76c70614a4bab00b0f6587d
Alexandria Cattron <acattron@gmail.com>
2017-01-28 17:36:13 -0800

    Refactored code in app init. Moved view related code to view manager.

--- client/app.js
+++ client/app.js
@@ -34,76 +34,17 @@ function App() {
 			document.getElementById('photos-container').addEventListener('click', function(e) {
 				const thumbImg = e.target;
 				if (thumbImg.nodeName === 'IMG') {
-					const photoUrl = thumbImg.getAttribute('data-photo-url');
-					const profileUrl = thumbImg.getAttribute('data-profile');
-					const photographer = thumbImg.getAttribute('data-photographer');
-
-					const photoViewerImg = document.querySelector('#photo-viewer img');
-					photoViewerImg.setAttribute('src', photoUrl);
-					if (thumbImg.width <= thumbImg.height) { //if image is, taller than it is wide
-						photoViewerImg.setAttribute('height', window.innerHeight * 0.9);
-					} else {
-						//smaller size than height adjustment to make room for arrows
-						photoViewerImg.setAttribute('width', window.innerWidth * 0.87);
-					}
-
-					const photosContainer = document.getElementById('photos-container');
-					const thumbImgIndex = [].indexOf.call(photosContainer.children, thumbImg.parentNode);
-					photoViewerImg.setAttribute('data-index', thumbImgIndex);
-
-					const photographerAttribution = `<a href="${profileUrl}">${photographer}</a>`;
-					document.querySelector('#photo-viewer span').innerHTML = 'Photo by ' +
-									photographerAttribution + ' / <a href="http://unsplash.com/">Unsplash</a>';
-
-					document.getElementById('modal').classList.remove('hidden');
-					view.resizeModal();
+					view.openPhotoViewer(thumbImg);
 				}
 			});
 			// close modal lightbox dialog
-			document.querySelector('#modal i').addEventListener('click', function () {
-				document.getElementById('modal').classList.add('hidden');
-				const photoViewerImg = document.querySelector('#photo-viewer img');
-				photoViewerImg.removeAttribute('height');
-				photoViewerImg.removeAttribute('width');
-				photoViewerImg.setAttribute('src', '');
-
-				document.getElementById('modal').style.height = 'auto';
-			});
+			document.querySelector('#modal i').addEventListener('click', view.closePhotoViewer.bind(view));
 
 			const arrowBtns = document.getElementsByClassName('arrow-btn');
 			for (let i = 0; i < arrowBtns.length; i++) { //should only be two arrow buttons
 				arrowBtns[i].addEventListener('click', function(e) {
-					const photoViewerImg = document.querySelector('#photo-viewer img');
-					photoViewerImg.setAttribute('src', '');
-					photoViewerImg.removeAttribute('height');
-					photoViewerImg.removeAttribute('width');
-					const shownImgIndex = parseInt(photoViewerImg.getAttribute('data-index'));
-					const photosList = document.getElementById('photos-container').children;
-
-					// show previous image if left arrow. show next image if right arrow.
-					const thumbImgIndex = (e.target.id === 'left-arrow') ?
-																shownImgIndex + photosList.length - 1 : shownImgIndex + 1;
-					// making list circular
-					const thumbImg = photosList[thumbImgIndex % photosList.length].firstChild;
-					
-					const photoUrl = thumbImg.getAttribute('data-photo-url');
-					const profileUrl = thumbImg.getAttribute('data-profile');
-					const photographer = thumbImg.getAttribute('data-photographer');
-
-					photoViewerImg.setAttribute('src', photoUrl);
-					if (thumbImg.width <= thumbImg.height) { //if image is, taller than it is wide
-						photoViewerImg.setAttribute('height', window.innerHeight * 0.9);
-					} else {
-						//smaller size than height adjustment to make room for arrows
-						photoViewerImg.setAttribute('width', window.innerWidth * 0.87);
-					}
-					photoViewerImg.setAttribute('data-index', thumbImgIndex);
-
-					const photographerAttribution = `<a href="${profileUrl}">${photographer}</a>`;
-					document.querySelector('#photo-viewer span').innerHTML = 'Photo by ' +
-									photographerAttribution + ' / <a href="http://unsplash.com/">Unsplash</a>';
-
-					view.resizeModal();
+					// sending an inc or dec
+					view.changePhotoViewerImg((e.target.id === 'left-arrow') ? -1 : 1);
 				});
 			}
 
--- client/view-manager.js
+++ client/view-manager.js
@@ -1,5 +1,26 @@
-
 function ViewManager() {
+	// PRIVATE HELPER METHOD
+	function updatePhotoViewer(photoViewerImg, thumbImg, thumbImgIndex) {
+		photoViewerImg.setAttribute('src', thumbImg.getAttribute('data-photo-url'));
+		if (thumbImg.width <= thumbImg.height) { //if image is, taller than it is wide
+			photoViewerImg.setAttribute('height', window.innerHeight * 0.9);
+		} else {
+			//smaller size than height adjustment to make room for arrows
+			photoViewerImg.setAttribute('width', window.innerWidth * 0.87);
+		}
+		photoViewerImg.setAttribute('data-index', thumbImgIndex);
+
+		const profileUrl = thumbImg.getAttribute('data-profile');
+		const photographer = thumbImg.getAttribute('data-photographer');
+		const photographerAttribution = `<a href="${profileUrl}">${photographer}</a>`;
+		document.querySelector('#photo-viewer span').innerHTML = 'Photo by ' +
+						photographerAttribution + ' / <a href="http://unsplash.com/">Unsplash</a>';
+
+		document.getElementById('modal').classList.remove('hidden');
+		this.resizeModal();
+	}
+
+	// OBJECT INSTANCE, PUBLIC METHODS
 	return {
 		replacePlaceHolderImages(json) {
 			const elems = document.querySelectorAll(".photo.empty");
@@ -9,16 +30,17 @@ function ViewManager() {
 				 * may not always be 20 (async updates of view)
 				 */
 				if (i < json.length) {
-					elems.item(i).firstChild.addEventListener('load', () => {
-						elems.item(i).classList.remove('empty');
+					const img = elems.item(i).firstChild;
+					img.addEventListener('load', () => {
+						img.parentNode.classList.remove('empty');
 					}, { once: true });
-					elems.item(i).firstChild.setAttribute('src', json[i].urls.thumb);
-				  elems.item(i).firstChild.setAttribute('data-color', json[i].color);
-				  elems.item(i).firstChild.setAttribute('data-photographer', json[i].user.name);
-				  elems.item(i).firstChild.setAttribute('data-profile', json[i].user.links.html);
-				  elems.item(i).firstChild.setAttribute('data-photo-url', json[i].urls.full);
+					img.setAttribute('src', json[i].urls.thumb);
+				  img.setAttribute('data-color', json[i].color);
+				  img.setAttribute('data-photographer', json[i].user.name);
+				  img.setAttribute('data-profile', json[i].user.links.html);
+				  img.setAttribute('data-photo-url', json[i].urls.full);
 				} else {
-					elems.item(i).remove();
+					img.parentNode.remove();
 				}
 			}
 			this.resizeModal();
@@ -50,6 +72,37 @@ function ViewManager() {
 					modal.style.height = `${document.body.clientHeight}px`;
 				}
 			}
+		},
+		openPhotoViewer(thumbImg) {
+			const photoViewerImg = document.querySelector('#photo-viewer img');
+			const photosContainer = document.getElementById('photos-container');
+			const thumbImgIndex = [].indexOf.call(photosContainer.children, thumbImg.parentNode);
+			
+			updatePhotoViewer.call(this, photoViewerImg, thumbImg, thumbImgIndex);
+		},
+		closePhotoViewer() {
+			const modal = document.getElementById('modal');
+			modal.classList.add('hidden');
+			modal.style.height = 'auto';
+			this.clearPhotoViewerImg();
+		},
+		clearPhotoViewerImg(photoViewerImg=document.querySelector('#photo-viewer img')) {
+			photoViewerImg.setAttribute('src', '');
+			photoViewerImg.removeAttribute('height');
+			photoViewerImg.removeAttribute('width');
+		},
+		changePhotoViewerImg(direction){
+			const photoViewerImg = document.querySelector('#photo-viewer img');
+			this.clearPhotoViewerImg(photoViewerImg);
+			const shownImgIndex = parseInt(photoViewerImg.getAttribute('data-index'));
+			const photosList = document.getElementById('photos-container').children;
+
+			// show previous image if left arrow. show next image if right arrow.
+			const thumbImgIndex = shownImgIndex + direction + ((direction < 0) ? photosList.length : 0);
+			// making list circular
+			const thumbImg = photosList[thumbImgIndex % photosList.length].firstChild;
+
+			updatePhotoViewer.call(this, photoViewerImg, thumbImg, thumbImgIndex);
 		}
 	};
 }
